815c0d5ec716dbcc8cb080986b0af7afa26b9df9
Otto Kafka <ottokafka@yahoo.com>
2020-07-01 10:50:11 +0800

    first up

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignUpClass(),
+    );
+  }
+}
+
+class SignUpClass extends StatelessWidget {
+  var email;
+  var password;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          automaticallyImplyLeading: false,
+        ),
+        child: SafeArea(
+            child: ListView(padding: const EdgeInsets.all(16), children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: CupertinoTextField(
+                  placeholder: "Email",
+                  onChanged: (value) {
+                    email = value;
+                  })),
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: CupertinoTextField(
+                  placeholder: "Password",
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  })),
+          FlatButton.icon(
+              onPressed: () {
+                print(email);
+                print(password);
+                signup(email, password);
+              },
+              icon: Icon(Icons.save),
+              label: Text("Sign UP"))
+        ])));
+  }
+}
+
+signup(email, password) async {
+  var url = "http://127.0.0.1:5000"; // iOS
+  final http.Response response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+      'password': password,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+  } else {
+    throw Exception('Failed to create album.');
+  }
+}
